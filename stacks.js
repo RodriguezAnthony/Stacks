f030a9f26bc5e9fd40d29134f6566eeee4769981
AntRod18 <antrod1895@gmail.com>
2021-12-02 01:04:54 -0500

    create node class and push fuction to add a new node to the end of stack

---
+++ stacks.js
@@ -0,0 +1,28 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = first;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else{
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp
+        }
+        this.size++;
+        return this.size;
+    }
+}
